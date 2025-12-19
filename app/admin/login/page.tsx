fd01adc6bcf80dde3cebd4bc2bc20605b31c6f8b
dan crowley <dancr.wley@gmail.com>
2025-12-19 15:18:18 -0500

    error handling on product creation, redirect from login if auth already

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -8,8 +8,8 @@ export default async function LoginPage() {
 
     const session = await auth()
 
-    if (!session) {
-        redirect('/admin/login')
+    if (session) {
+        redirect('/admin')
     }
 
     return (
