c50e0931028e90b1b715bb2242e5acaa04f0436a
dan crowley <dancr.wley@gmail.com>
2026-02-05 13:52:48 -0500

    prismaaaA

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
+// @ts-expect-error: Prisma 5 internal flag for serverless
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-      log: ['query', 'info', 'warn', 'error'], // optional for debugging
+      log: ['query', 'info', 'warn', 'error'],
+      __internal: {
+        usePreparedStatements: false,
+      },
     })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
